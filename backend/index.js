0dd26e2c42c6090476248c682eb02412a5f97dca
Hirusha Sasanka <IT21352676@my.sliit.lk>
2024-09-06 01:26:53 +0530

    index changes

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const serverless = require('serverless-http');
 
 dotenv.config();
 
@@ -37,3 +38,13 @@ app.listen(PORT, () => {
 // Start the Discord bot
 const startBot = require('./bot');
 startBot();
+
+const express = require('express');
+const serverless = require('serverless-http');
+
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from the backend!' });
+});
+
+module.exports.handler = serverless(app);
